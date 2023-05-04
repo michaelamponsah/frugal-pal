32f93d75471cd23cfb7b54e0e01b31df7705f6bb
michaelamponsah <amponsahmichaelkwame@gmail.com>
2023-05-04 21:14:22 +0000

    Add seed data for testing

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,37 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+User.destroy_all
+Group.destroy_all
+
+User.create([
+  {
+    name: "Stephen Greens",
+    email: "stephengreen@gmail.com",
+    password: "test123"
+  },
+  {
+    name: "Wes Gibbins",
+    email: "wesgibbins@gmail.com",
+    password: "test123"
+  }
+])
+
+Group.create([
+  {
+    name: "Utility Bills",
+    icon: "fa-solid fa-plug",
+    user_id: 1
+  },
+  {
+    name: "Rent",
+    icon: "fa-solid fa-house-buildin",
+    user_id: 1
+  },
+  {
+    name: "Groceries",
+    icon: "fa-solid fa-cart-shopping",
+    user_id: 1
+  },
+])
