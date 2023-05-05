a7b3bf97efe8ebc9baba4e41851b117c5abea44b
michaelamponsah <amponsahmichaelkwame@gmail.com>
2023-05-05 21:24:43 +0000

    Implement action to create a new category

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -13,4 +13,25 @@ class GroupsController < ApplicationController
 
     render
   end
+
+  def new
+    @group = Group.new
+  end
+
+  def  create
+    @new_category = Group.new(req_params)
+    @new_category.user_id = current_user.id
+
+    if @new_category.save
+      redirect_to groups_path, notice: "#{@new_category.name} Category has been created successfully"
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def req_params
+    params.require(:group).permit(:name, :icon)
+  end
 end
