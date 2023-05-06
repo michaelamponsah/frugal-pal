3a771c7467ee1a3bf3a0010ce9bb8404875688ea
michaelamponsah <amponsahmichaelkwame@gmail.com>
2023-05-06 14:04:00 +0000

    Implement logout feature

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :users
   root "home#index"
+  devise_for :users
+
+  devise_scope :user do
+    get  '/users/sign_out' => 'devise/sessions#destroy'
+  end
 
   resources :groups, only: [:index, :new, :destroy, :show, :create] do
     resources :expenses, only: [:new, :create]
